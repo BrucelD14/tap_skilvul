5532878aa728b54b9657fcfa2d1b5726181e9f55
BrucelD14 <samuderaduta144@gmail.com>
2022-10-08 20:55:48 +0700

    fixing error, change formula BMI, change result info

--- tpa2/script.js
+++ tpa2/script.js
@@ -1,6 +1,8 @@
 const button = document.getElementById('btn'); 
 
-button.addEventListener('click', () => {
+button.addEventListener('click', (ev) => {
+    
+    ev.preventDefault()
     const weight = parseInt(document.getElementById('weight').value);
     const height = parseInt(document.getElementById('height').value);
     const result = document.getElementById('result-bmi');
@@ -22,14 +24,16 @@ button.addEventListener('click', () => {
     }
 
     if(height_status && weight_status){
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        let bmi = (weight / ((height / 100)^2)).toFixed(2);
 
         if(bmi < 18.6){
-            result.innerHTML = "under weight : " + bmi;
-        }else if(bmi >= 18.6 && bmi < 24.9){
-            result.innerHTML = "normal : " + bmi;
+            result.innerHTML = `Your BMI is ${bmi}, which means You are Overweight`;
+        }else if(bmi >= 18.6 && bmi <= 24.9){
+            result.innerHTML = `Your BMI is ${bmi}, which means You are Normal`;
+        }else if(bmi >= 25 && bmi <= 29.9){
+            result.innerHTML = `Your BMI is ${bmi}, which means You are Overweight`;
         }else{
-            result.innerHTML = "over weight : " + bmi;
+            result.innerHTML = `Your BMI is ${bmi}, which means You are Obesity` ;
         }
     }else{
         alert("the form has errors");
