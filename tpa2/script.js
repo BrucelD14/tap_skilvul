2e351f51a35097985b909c7d537067f106761ac1
BrucelD14 <samuderaduta144@gmail.com>
2022-11-08 17:09:49 +0700

    fixed bug

--- tpa2/script.js
+++ tpa2/script.js
@@ -27,7 +27,7 @@ button.addEventListener('click', (ev) => {
         let bmi = (weight / ((height / 100)^2)).toFixed(2);
 
         if(bmi < 18.6){
-            result.innerHTML = `Your BMI is ${bmi}, which means You are Overweight`;
+            result.innerHTML = `Your BMI is ${bmi}, which means You are Underweight`;
         }else if(bmi >= 18.6 && bmi <= 24.9){
             result.innerHTML = `Your BMI is ${bmi}, which means You are Normal`;
         }else if(bmi >= 25 && bmi <= 29.9){
